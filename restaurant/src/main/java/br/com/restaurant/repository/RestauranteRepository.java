5a49a25c6dc9c9c77a0ff04f895847be429b6d4f
Will <143566517+kuchilasaske2@users.noreply.github.com>
2024-06-15 19:38:24 -0300

    Delete restaurant directory

--- restaurant/src/main/java/br/com/restaurant/repository/RestauranteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.restaurant.model.Restaurante;
-
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {}
--- restaurant/src/main/java/br/com/restaurant/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.restaurant.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
